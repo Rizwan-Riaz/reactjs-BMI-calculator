c86ca237a7ce99b9901edc1d30f1c3f34a459b20
Rizwan-Riaz <rizwanriaz1999@gmail.com>
2023-05-18 13:12:12 +0500

    created with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import { useState,useMemo } from 'react';
 import './App.css';
+const DEFUALT_WEIGHT = 50;
+const DEFUALT_HEIGHT = 100;
+export default function App() {
+  const [weight, setWeight] = useState(DEFUALT_WEIGHT);
+  const [height, setHeight] = useState(DEFUALT_HEIGHT);
 
-function App() {
+  function onHeightChange(event) {
+    const inputHeight = event.target.value;
+    setHeight(inputHeight);
+  }
+  function onWeightChange(event){
+    const inputWeight = event.target.value;
+    setWeight(inputWeight);
+  }
+
+  const output = useMemo(()=>{
+    const calculateHeight = height / 100;
+    return(
+      weight / (calculateHeight * calculateHeight)).toFixed(1); 
+  },[ height , weight ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <main>
+    <h1>PROJECT : BMI CALCULATOR</h1>
+    <div className='input-section'>
+       <p className='slider-output'> Weight:{weight}kg</p>
+        <input
+        className='input-slider'
+        onChange={onWeightChange}
+        type='range'
+        step="1"
+        min = "40"
+        max = "200"
+        />
+
+        <p className='slider-output'>Height : {height}cm</p>
+        <input
+        className='input-slider'
+        onChange={onHeightChange}
+        type='range'
+        min='120'
+        max = "220"
+        />
     </div>
+
+    <div className='output-section'>
+          <p>Your BMI is:</p>
+          <p className='output'>{output}</p>
+    </div>
+    </main>
   );
 }
-
-export default App;
